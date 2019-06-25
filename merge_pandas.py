a8a97634b83cadd7e54562b128ed4bbb3a416e21
bscheibel <beate.scheibel@gmail.com>
2019-06-25 12:07:21 +0200

    merge csv rows

--- merge_pandas.py
+++ merge_pandas.py
@@ -1,4 +1,4 @@
 import pandas
-df = pandas.read_csv('text.csv', header = 0)
-df['Total'] = df.groupby(['Name', 'Age'])['Salary'].transform('sum')
-df.drop_duplicates(take_last=True)
+df = pandas.read_csv('text.csv', header = 0, delimiter=";")
+df['Total'] = df.groupby(['x'])['Text'].transform('sum')
+df.drop_duplicates()
