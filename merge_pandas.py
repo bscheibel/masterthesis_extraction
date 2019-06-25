18b764b5d0218b80222dce683bf11223db43e4a3
bscheibel <beate.scheibel@gmail.com>
2019-06-25 12:14:16 +0200

    merge csv rows

--- merge_pandas.py
+++ merge_pandas.py
@@ -1,4 +1,5 @@
 import pandas
 df = pandas.read_csv('text.csv', header = 0, delimiter=";")
-df['Total'] = df.groupby(['x'])['Text'].transform('sum')
+df['Total'] = df.groupby(['x','y'])['Text'].transform('sum')
 df.drop_duplicates()
+print(df)
