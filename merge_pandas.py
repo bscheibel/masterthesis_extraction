3a5a13af26ddbf91f583221a8e51c2218a168c67
beatescheibel <beate.scheibel@gmail.com>
2019-06-27 09:15:23 +0200

    print text

--- merge_pandas.py
+++ merge_pandas.py
@@ -1,5 +1,5 @@
 import pandas
 df = pandas.read_csv('text.csv', header = 0, delimiter=";")
-df['Text'] = df.groupby(['x','y'])['Text'].transform('sum')
+df['Text'] = df.groupby(['X','Y'])['TEXT'].transform('sum')
 df.drop_duplicates()
 df.to_csv("merged.csv")
