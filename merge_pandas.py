2a56eca18214a997dc81086d23af49b208b513d3
bscheibel <beate.scheibel@gmail.com>
2019-07-08 07:09:33 +0200

    laufer test

--- merge_pandas.py
+++ merge_pandas.py
@@ -4,4 +4,4 @@ def merge_lines(file_out):
     df = pandas.read_csv(file_out, header = 0, delimiter=";")
     df['Text'] = df.groupby(['X','Y'])['TEXT'].transform('sum')
     df.drop_duplicates()
-    df.to_csv("file_out.csv")
+    df.to_csv(file_out)
