9b527a49c8d67344ba4217ed2c58550e5d26fc92
bscheibel <beate.scheibel@gmail.com>
2019-07-24 14:53:39 +0200

    started with regex extraction

--- regex_extraction.py
+++ regex_extraction.py
@@ -2,6 +2,8 @@
 import re
 
 regex = r"(\S+\s{1,3}?\S*\s?\S*\S*\s?\S*\S*\s?\S*\S*\s?\S*\S*\s+)"
+regex1 = r"([A-Z]\s?\W\s?\d\d?\s?\s?\W\s?\d\d?\s?\W)" #ti get the bezeichnungen raus
+regex2= r"([a-zA-Z]{3,})" #alle wörter raus???
 extracted_dimensions = []
 file=open('/home/bscheibel/PycharmProjects/dxf_reader/drawings/5152166_Rev04.txt', 'r')
 text= file.read()
@@ -9,6 +11,7 @@ file.close()
 matches = re.findall(regex, text, re.MULTILINE)
 for match in matches:
         extracted_dimensions.append(match.strip())
+
 
 
 print(extracted_dimensions)
