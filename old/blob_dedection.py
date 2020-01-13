a893e8d19e8caf23cb0717675fd523b1be34d54b
bscheibel <beate.scheibel@gmail.com>
2020-01-13 11:26:34 +0100

    cleaned folder structure

--- old/blob_dedection.py
+++
@@ -1,5 +0,0 @@
-import text_to_image
-encoded_image_path = text_to_image.encode_file("/home/bscheibel/PycharmProjects/engineering_drawings_extraction/drawings/5129275_Rev01-GV12.txt", "output_image.png")
-
-import imgkit
-imgkit.from_file('/home/bscheibel/PycharmProjects/engineering_drawings_extraction/drawings/5129275_Rev01-GV12.html', 'out.jpg')
